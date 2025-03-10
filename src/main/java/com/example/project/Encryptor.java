2421544ec53dcf10f5616a2b89bb06818541696a
AR5616 <abdurrahmanm2@nycstudents.net>
2025-03-10 19:46:42 +0000

    complete

--- src/main/java/com/example/project/Encryptor.java
+++ src/main/java/com/example/project/Encryptor.java
@@ -5,19 +5,65 @@ import java.util.Arrays;
 public class Encryptor {
     
     public static int determineColumns(int messageLen, int rows){
-        
-        return 0;
+        if (messageLen == 0) {
+            return 1;
+        }
+        if (messageLen % rows == 0) {
+            return messageLen/rows;
+        }
+        return (messageLen/rows) + 1;
     }
     
     public static String[][] generateEncryptArray(String message, int rows) {
-        return new String [0][0];
+        int messageSub = 0;
+        String[][] encryption = new String[rows][determineColumns(message.length(), rows)];
+        for (int i = 0; i < encryption.length; i++) {
+            for (int j = 0; j < encryption[0].length; j++) {
+                if (messageSub >= message.length()) {
+                    encryption[i][j] = "=";
+                } else {
+                encryption[i][j] = message.substring(messageSub, messageSub + 1);
+                messageSub++;
+                }
+            }
+        }
+        return encryption;
     }
 
-    public static String encryptMessage(String message, int rows){
-        return "";
+    public static String encryptMessage(String message, int rows) {
+        String[][] encryption = generateEncryptArray(message, rows);
+        String encryptionString = "";
+        for (int i = encryption[0].length - 1; i >= 0; i--) {
+            for (int j = 0; j < encryption.length; j++) {
+                encryptionString += encryption[j][i];
+            }
+        }
+        return encryptionString;
     }
 
     public static String decryptMessage(String encryptedMessage, int rows) {
-        return "";
+        String decryption = "";
+        int encryptedSub = 0;
+        String[][] encryptedDecryp = new String[rows][determineColumns(encryptedMessage.length(), rows)];
+        if (rows * determineColumns(encryptedMessage.length(), rows) < encryptedMessage.length()) {
+            return "";
+        }
+        for (int i = determineColumns(encryptedMessage.length(), rows) - 1; i >= 0; i--) {
+            for (int j = 0; j < rows; j++) {
+                if (encryptedSub < encryptedMessage.length()) {
+                    encryptedDecryp[j][i] = encryptedMessage.substring(encryptedSub, encryptedSub + 1);
+                    encryptedSub++;
+                }
+            }
+        }
+
+        for (int i = 0; i < encryptedDecryp.length; i++) {
+            for (int j = 0; j < encryptedDecryp[0].length; j++) {
+                if (encryptedDecryp[i][j] != null && !encryptedDecryp[i][j].equals("=")) {
+                    decryption += encryptedDecryp[i][j];
+                }
+            }
+        }
+        return decryption;
     }
 }
---
+++ src/main/java/com/example/project/Testing.java
@@ -0,0 +1,9 @@
+package com.example.project;
+
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println(Encryptor.decryptMessage("Bnasshgasbkblleoa m!===eoJeit ee stlpy  li!===Lr m  erttaea arflte!==", 23));
+
+    }
+
+}
